8f1ba00dd1d50a556843bfc508d1ecd71c9078fd
radubaciuDell <radumbaciu@gmail.com>
2022-05-21 16:06:14 +0300

    UI working on secondary pages

---
+++ src/main/java/us/icarenow/portal/controller/SignInController.java
@@ -0,0 +1,14 @@
+package us.icarenow.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("pages")
+public class SignInController {
+    @GetMapping("sign-in")
+    private String getSignInPage(){
+        return "pages/sign-in.html";
+    }
+}
