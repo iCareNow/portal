f7c7d23c01e72f6c6044d8beb26cb6a4e6115266
radumbaciu <radumbaciu@gmail.com>
2022-05-23 17:31:55 +0300

    working welcome page, sign in, sign up & connections

--- src/main/java/us/icarenow/portal/controller/PagesController.java
+++ src/main/java/us/icarenow/portal/controller/PagesController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("pages")
+@RequestMapping("")
 public class PagesController {
-    @GetMapping("sign-in")
+    @GetMapping("sign-in.html")
     private String getSignInPage(){
-        return "pages/sign-in.html";
+
+        return "sign-in.html";
+    }
+    @GetMapping("sign-up.html")
+    private String getSignUpPage(){
+
+        return "sign-up.html";
     }
 }
